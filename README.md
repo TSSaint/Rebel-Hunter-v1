10832aebbb2fc4c427a3c7fee29c3c777f9e0312
Thomas Santos <thomas@socialeffort.co>
2017-07-16 21:05:41 -0400

    MVP commit

--- README.md
+++ README.md
@@ -1,23 +1,31 @@
-# [Name of Game]
+# Rebel Hunter
 
-[screenshot]
+![](./rebelhunterv1/openingshot.jpg)
 
-## What is [name of game]?
+## What is Rebel Hunter?
 
-> Brief discussion of what the game is and why you chose to make it
+> Rebel Hunter is a single player, 1D side scroller where users play as Darth Vader to eliminate rebel forces as they try to escape before their energy/depletes to 0. The game is not designed to have a clear win-lose scenario. It is more of a rhythm/survival game. A user can block incoming blaster bolts and kill enemy agents in order to rack up points, whilst trying to last as long as possible.
 
 ## Technical Discussion
 
-> List of the technologies you're using (HTML, CSS, JavaScript)
+> This project uses a structured approach to design with HTML and CSS. Also, it uses JS/jQuery for DOM manipulation, constructors, random spawn and time intervals of agents, arrow functions to simplify syntax, for/if-else statements to describe collision logic, and to reset the game world upon user death, among other functions. 
 
 ### Notes on Game Structure
 
-> Code samples, description of challenges you overcame, etc.
+> The first approach I used was to test how the game would look. This was done early in the design process with a quickly written HTML/CSS syntax and minor JS dom manipulation. After getting approval from the group leader, I started to fully work on the structure of the game by essentially dividing the game into 3 states - "start" -> "gameplay" -> "end", which also helped immensely for CSS styling and spawning the game pieces in a maneagable way. The most time consuming aspect of the game was researching a good way to approach game-making, facing questions such as "what is considered enjoyable", "how do you create a lifelike approach to movements", "how do you reward a player for sticking it through", and "how can you tell a story", to name a few. This resulted in creating constructors that spawned the rebel and blaster bolt agents via a constructor and calling them at game start, to create a sense of "overwhelming odds" for the player. I played with the spawn rates and gameplay speeds/hitboxes to get a good balance between "Okay, I can breeze through this" and "I can be easily overwhelmed if I miss a beat". This game is essentially a good balance between a rhythm game and a survival game sprinkled with a little love for Star Wars. I admit it's not perfect, but I tried to consider what design and game making principles I could, along with working exactly off of the templates given in the past 3 weeks' lectures and homeworks. In fact, while thinking about the game on my off-hours, I realized that it might have been just-right for the game to feel the way it might - after all, Darth Vader has a reputation for eliminating rebels with ease.
 
-## The Making of [Your Game]
+## The Making of Rebel Hunter
 
-> Any credits or notes you feel you should add
+> I used a combination of syntax/style guides provided by the instructors in Unit 1, stackoverflow/MDN/w3 school references easily available online (referenced in the javascript comments) to help guide the design process and figure out the best way to solve problems. The result was almost totally revising my initial approach to create this game.
 
 ## Opportunities for Future Growth
 
-> If you had more time to work on your game, what would you do?
+> Without a doubt, I would add the following features to the game if I knew how to implement it fast enough/had incentive to:
+	- Add complex character animations (movement, shooting, death, etc.)
+	- Add several new attacks, like throw lightsaber, force crush, and GodMode
+	- Congratulate user on performance (3k points or more special ending message)
+	- Add high-scores
+	- Add pause menu
+	- Add music and sounds to add to the Star Wars experience
+	- Add different kinds of enemies
+	- Add different kinds of maps/locations
--- js/main.js
+++ js/main.js
@@ -29,6 +29,7 @@
 //   |                                 |                    [.]b       //
 //___|_________________________________|____________________[ ]________//
 
+// logic for random spawning
 function getRandomInt(min, max) { // >>>https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -36,58 +37,57 @@ function getRandomInt(min, max) { // >>>https://developer.mozilla.org/en-US/docs
 }
 
 // constructor ENEMY
-const Enemy = function () { // each one of these enemy has properties holding health,
-  this.element = null;
-  this.health = 100;        // size of hitbox, and position
-  this.position = {
-    x: 1080
+const Enemy = function () { // each enemy has properties holding health,
+  this.element = null;      // exists or not
+  // this.health = 100;     // enemy just dies on hit, maybe add health in the future
+  this.position = {         // observes enemy position on X
+    x: 1080                 // starts off the map to look more seamless
   };
-  this.intervalId = null;
-
-  this.create();
-  this.move();
+  this.intervalId = null;   // exists or not
+  this.create();            // summons enemy agents
+  this.move();              // forces enemies to move and spawn at random
 };
 
 Enemy.prototype.create = function () {
-  $('.rebel-holder').append('<div class="rebel-soldier"></div>');
-  this.element = $('.rebel-soldier').last();
-};
+  $('.rebel-holder').append('<div class="rebel-soldier"></div>'); // create divs inside class rebel-holder
+  this.element = $('.rebel-soldier').last(); // recognizes existing instances of rebel-soldier; returns the last element of existence
+};                                           // https://www.w3schools.com/jquery/traversing_last.asp
 
-Enemy.prototype.move = function () {
+Enemy.prototype.move = function () { // makes the bad guys move
   this.intervalId = setInterval (() => {
-    this.position.x -= 20;
-    this.element.css('left', this.position.x + 'px');
-  }, 200);
+    this.position.x -= 4; // still can't decide on a good rhythm; decided between 2 and 10
+    this.element.css('left', this.position.x + 'px'); // tells
+  }, 20); // still can't decide on a good rhythm; decided between 40 and 200
 };
 
 Enemy.prototype.die = function () {
   // >>>https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearInterval
-  clearInterval(this.intervalId); // stop moving
-  this.element.remove();
-};
+  clearInterval(this.intervalId); // 'stop moving' so to speak; eliminates them from the world/variable intervalId
+  this.element.remove(); // this was important to add as not doing so kept
+};                       // the enemy 'ghosted' and player keeps getting points as result
 
 //constructor BOLT
-const Bolt = function () { // each one of these enemy has properties holding health,
+const Bolt = function () { // each one of these are blaster bolts
   this.element = null;
-  this.position = {
+  this.position = {        // starts off the map to look more seamless
     x: 1080
   };
   this.intervalId = null;
 
-  this.create();
+  this.create(); // same as notes in Enemy constructor
   this.move();
 };
 
 Bolt.prototype.create = function () {
-  $('.bolt-holder').append('<div class="bolt"></div>');
+  $('.bolt-holder').append('<div class="bolt"></div>'); // create divs inside class bolt-holder
   this.element = $('.bolt').last();
 };
 
-Bolt.prototype.move = function () {
+Bolt.prototype.move = function () { // see notes on Rebel mover
   this.intervalId = setInterval (() => {
     this.position.x -= 5;
     this.element.css('left', this.position.x + 'px');
-  }, 10);
+  }, 5);
 };
 
 Bolt.prototype.die = function () {
@@ -99,22 +99,22 @@ Bolt.prototype.die = function () {
 // constructor PLAYER
 const Player = function () { // holds instance of Darth Vader
   this.element = $('#darth-vader');
-  this.health = 100;
-  this.position = {
+  this.health = 100; // starting health - can deplete to 0
+  this.position = { // starting position of vader
     x: 0
   };
 };
 
 Player.prototype.moveLeft = function () {
-  if (this.position.x > 0) { // stop vader from going LEFT off screen
-    this.position.x -= 30;
+  if (this.position.x > 0) { // stops vader from going LEFT off screen
+    this.position.x -= 30; // allows all other movement to left direction to follow this speed
     this.element.css('left', this.position.x + 'px');
   }
 };
 
 Player.prototype.moveRight = function () {
-  if (this.position.x < 903) { // stop vader from going RIGHT off screen
-    this.position.x += 30;
+  if (this.position.x < 903) { // stops vader from going RIGHT off screen
+    this.position.x += 30; // allows all other movement to right direction to follow this speed
     this.element.css('left', this.position.x + 'px');
   }
 };
@@ -123,30 +123,36 @@ Player.prototype.moveRight = function () {
 // constructor GAME
 const Game = function () {
   this.score = 0;     // add this. -- and = Num;
-  this.enemies = [];  // add this. -- and = [];
-  this.bolts = [];
+  this.enemies = [];  // add this. -- and = []; holds enemy instances.
+  this.bolts = [];  // holds bolt instances.
   this.player = new Player();
-  this.addListeners();
-  this.listenForBolts();
+  this.addListeners(); // listens for movements
+  this.listenForBolts(); // listens for bolts
 };
 
 // player start and replay buttons set here
-Game.prototype.addListeners = function () {
-  $('.action-begin, .action-again').click(() => {
+Game.prototype.addListeners = function () { // starts THE GAME!
+  $('.action-begin').click(() => {
     $('.screen').removeClass('active');
     $('#game-screen').addClass('active');
     this.start();
   });
 
-  // replay button only
-  $('.action-main').click(function () {
+  $('.action-again').click(() => { // returns to THE GAME after it ended (need to make a way to pause)
+    $('game-screen div').html('');
     $('.screen').removeClass('active');
-    $('#start-screen').addClass('active');
+    $('#game-screen').addClass('active');
+    this.start();
   });
 
-  $(document).on('keypress', (event) => { // whenever user clicks these, do it
-    if (event.which === 32) {
-      // spacebar
+  // return to main
+  $('.action-main').click(function () { // returns to the main menu and resets the world
+    history.go();
+  });
+
+  $(document).on('keypress', (event) => { // whenever user clicks these, do it (pretty self-explanatory)
+    if (event.which === 32) {             // each instance of this exists in the constructor player,
+      // spacebar                         // can be changed to be other keys as well
       this.playerAttacks();
     } else if (event.which === 49) {
       // 1
@@ -161,106 +167,113 @@ Game.prototype.addListeners = function () {
   });
 };
 
-Game.prototype.listenForBolts = function () {
-  const boltWidth = 100;
-  const vaderWidth = 177;
+Game.prototype.listenForBolts = function () { // first piece of collision logic
+  const boltWidth = 100;                      // sets the size of the bolt, approx. to it's size in CSS
+  const vaderWidth = 177;                     // sets the size of Vader's hitbox
   setInterval(() => {
-    let vaderA = this.player.position.x;
-    let vaderB = vaderA + vaderWidth;
-    for (var i = 0; i < this.bolts.length; i++) {
-      let boltA = this.bolts[i].position.x;
-      let boltB = boltA + boltWidth;
-      let isVaderAWithinBolt = vaderA >= boltA && vaderA <= boltB;
-      let isVaderBWithinBolt = vaderB >= boltA && vaderB <= boltB;
+    let vaderA = this.player.position.x; // holds player initial poisition
+    let vaderB = vaderA + vaderWidth; // player's hitbox size
+    for (var i = 0; i < this.bolts.length; i++) { // applies to each instance of bolt that is produced by game constructor
+      let boltA = this.bolts[i].position.x; // starting position of bolt
+      let boltB = boltA + boltWidth; // bolt's hitbox size
+      let isVaderAWithinBolt = vaderA >= boltA && vaderA <= boltB; // compares area position of player and bolt
+      let isVaderBWithinBolt = vaderB >= boltA && vaderB <= boltB; // compares area position of player and bolt
       if (isVaderAWithinBolt || isVaderBWithinBolt ) { // if collision
-        this.bolts[i].die();
-        this.bolts.splice(i, 1);
-        this.subtractHealth(10);
+        this.bolts[i].die(); // bolt disappears from DOM
+        this.bolts.splice(i, 1); // lose an item in the array bolts
+        this.subtractHealth(10); // player loses HP - cant' decide but a good range is 3 - 20
       }
     }
-  }, 250);
+  }, 250); // time of collision checking
 };
 
-Game.prototype.playerAttacks = function () {
-  const enemyWidth = 120;
+Game.prototype.playerAttacks = function () { // holds player attacks initial position
+  const enemyWidth = 120; // sets the size of the enemy, approx. to it's size in CSS
   const vaderWidth = 177;
-  const hitboxWidth = 40;
-  let hitboxA = this.player.position.x + vaderWidth;
-  let hitboxB = hitboxA + hitboxWidth;
-  for (var i = 0; i < this.enemies.length; i++) {
-    let enemyA = this.enemies[i].position.x;
-    let enemyB = enemyA + enemyWidth;
-    let isHitboxAWithinEnemy = hitboxA >= enemyA && hitboxA <= enemyB;
-    let isHitboxBWithinEnemy = hitboxB >= enemyA && hitboxB <= enemyB;
+  const hitboxWidth = 60; // how big vader's lightsaber swing is
+  let hitboxA = this.player.position.x + vaderWidth; // this is where player is at any given time
+  let hitboxB = hitboxA + hitboxWidth; // size of Vader's lightsaber swing kill box
+  for (var i = 0; i < this.enemies.length; i++) { // loops through all instances of enemies
+    let enemyA = this.enemies[i].position.x; // holds instances of Rebel soldiers
+    let enemyB = enemyA + enemyWidth; // size of Rebel soldier's hitbox
+    let isHitboxAWithinEnemy = hitboxA >= enemyA && hitboxA <= enemyB; // compares attack to position of enemy
+    let isHitboxBWithinEnemy = hitboxB >= enemyA && hitboxB <= enemyB; // compaes attack to position of enemy
     if (isHitboxAWithinEnemy || isHitboxBWithinEnemy ) { // if collision
-      this.enemies[i].die();
+      this.enemies[i].die(); // enemy disappears from DOM
       this.enemies.splice(i, 1); // https://stackoverflow.com/questions/5767325/how-do-i-remove-a-particular-element-from-an-array-in-javascript
-      this.addToScore(500);
+      this.addToScore(1000);
     }
   }
   // >>> LETS ADD: https://stackoverflow.com/questions/14290424/game-programming-how-to-add-delay-between-attacks
 };
 
-Game.prototype.playerBlocks = function () {
+Game.prototype.playerBlocks = function () { // works the same as player blocks but eliminates bolts
   const boltWidth = 100;
   const vaderWidth = 177;
   const hitboxWidth = 40;
   let hitboxA = this.player.position.x + vaderWidth;
   let hitboxB = hitboxA + hitboxWidth;
+
   for (var i = 0; i < this.bolts.length; i++) {
     let boltA = this.bolts[i].position.x;
     let boltB = boltA + boltWidth;
     let isHitboxAWithinBolt = hitboxA >= boltA && hitboxA <= boltB;
     let isHitboxBWithinBolt = hitboxB >= boltA && hitboxB <= boltB;
     if (isHitboxAWithinBolt || isHitboxBWithinBolt ) { // if collision
-      this.bolts[i].die();
+      this.bolts[i].die(); // bolt disappears from DOM
       this.bolts.splice(i, 1);
-      this.addToScore(100);
+      this.addToScore(250);
     }
   }
   // >>> LETS ADD: https://stackoverflow.com/questions/14290424/game-programming-how-to-add-delay-between-attacks
 };
 
-Game.prototype.addToScore = function (points) {
-  this.score += points;
-  $('#game-score span').text(this.score);
+Game.prototype.addToScore = function (points) { // rewards the player's well-timed moves
+  this.score += points; // adds points
+  let inGame = $('#game-score span').text(this.score); // updates gameplay's score
+  $('#game-score-end span').text(inGame); // doesn't work as of yet
 };
 
-Game.prototype.subtractHealth = function (damage) {
-  this.player.health -= damage;
-  $('#game-health span').text(this.player.health);
-  if (this.player.health <= 0) {
-    $('.screen').removeClass('active');
-    $('#end-screen').addClass('active');
+Game.prototype.subtractHealth = function (damage) { // punish player for getting shot by bolts
+  this.player.health -= damage; // deduct health
+  $('#game-health span').text(this.player.health); // updates player health
+  if (this.player.health <= 0) { // sets condition for "death" or game end. But Vader can't really die so it just ends
+    $('.screen').removeClass('active'); // changes game state
+    $('#end-screen').addClass('active'); // see above ^
   }
 };
 
-Game.prototype.start = function () {
-  this.createEnemy();
-  this.createBolt();
+Game.prototype.start = function () { // occurs at start of game
+  this.createEnemy(); // spawn Rebels
+  this.createBolt(); // spawn blaster fire
 };
 
 Game.prototype.createEnemy = function () {
-  var enemy = new Enemy(); // creates new bad guys!!!!
+  var enemy = new Enemy(); // creates new bad guys
   this.enemies.push(enemy); // looks at Game constructor, and calls 1 new bad guy
 
-  let randomTime = getRandomInt(500, 5000);
+  let randomTime = getRandomInt(1000, 5000); // sets interval of Rebel spawn
   setTimeout(() => {
     this.createEnemy();
   }, randomTime); // >>>https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions#Recursion
 };
 
 Game.prototype.createBolt = function () {
-  var bolt = new Bolt(); // creates new bad guys!!!!
-  this.bolts.push(bolt); // looks at Game constructor, and calls 1 new bad guy
+  var bolt = new Bolt(); // creates new bolts
+  this.bolts.push(bolt); // looks at Game constructor, and calls 1 new blaster bolt
 
-  let randomTime = getRandomInt(500, 2000);
+  let randomTime = getRandomInt(500, 1000);
   setTimeout(() => {
     this.createBolt();
   }, randomTime); // >>>https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions#Recursion
 };
 
-const game = new Game();
+const game = new Game(); // gives birth to a new instance of the game
+
+
+// random notes of the week
+// const audio = new Audio('../RebelHunterv1/breathing.mp3');
+// audio.play();
 // end
 
 // background
